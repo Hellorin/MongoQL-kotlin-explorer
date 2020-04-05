6972b41204c9aeb9b7ad4c028ccec58d5e6b6036
David Lawrence <davi.Lawrence@gmail.com>
2020-04-05 18:17:34 +0200

    Added scm informations

--- src/main/kotlin/io/github/hellorin/graphqlmongoexplorer/Application.kt
+++ src/main/kotlin/io/github/hellorin/graphqlmongoexplorer/Application.kt
@@ -1,8 +1,10 @@
 package io.github.hellorin.graphqlmongoexplorer
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
